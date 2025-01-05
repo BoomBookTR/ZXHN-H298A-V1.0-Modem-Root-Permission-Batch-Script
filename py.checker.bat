41ccc928d91c72c9568402dd96429681c2380879
BoomBookTR <10184695+BoomBookTR@users.noreply.github.com>
2025-01-05 20:40:31 +0300

    Add files via upload

--- py.checker.bat
+++ py.checker.bat
@@ -12,4 +12,18 @@ if %errorlevel% neq 0 (
     echo "py komutu calisiyor."
 )
 echo ------- -------
+
+
+python --version
+
+pip list
+
+echo ------- -------
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo "python komutu bulunamadi veya calismiyor."
+) else (
+    echo "python komutu calisiyor."
+)
+echo ------- -------
 pause
