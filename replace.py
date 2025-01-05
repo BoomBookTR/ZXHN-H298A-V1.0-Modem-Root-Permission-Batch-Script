0114358feedab3a1dc722c8484e209c9eb221361
BoomBookTR <10184695+BoomBookTR@users.noreply.github.com>
2025-01-05 19:47:17 +0300

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,8 +1,11 @@
-fin = open("config.xml", "rt")
+# Dosyayı UTF-8 kodlamasıyla açıyoruz
+fin = open("config.xml", "rt", encoding="utf-8")
 data = fin.read()
-data = data.replace('<DM name="ViewName" val="IGD.AU1"/>'+"\n"+'<DM name="Enable" val="0"/>', '<DM name="ViewName" val="IGD.AU1"/>'+"\n"+'<DM name="Enable" val="1"/>')
+data = data.replace('<DM name="ViewName" val="IGD.AU1"/>' + "\n" + '<DM name="Enable" val="0"/>', '<DM name="ViewName" val="IGD.AU1"/>' + "\n" + '<DM name="Enable" val="1"/>')
 data = data.replace('<DM name="SwitchShellEnable" val="0"/>', '<DM name="SwitchShellEnable" val="1"/>')
 fin.close()
-fin = open("config.xml", "wt")
+
+# Dosyayı yazarken de UTF-8 kodlamasını kullanıyoruz
+fin = open("config.xml", "wt", encoding="utf-8")
 fin.write(data)
 fin.close()
